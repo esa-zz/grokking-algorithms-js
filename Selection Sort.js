c5f7a1b720765d755c93e2abf02924eed4518a3c
ignonoko <essanezhad@proton.me>
2024-04-24 03:37:13 +0330

    selection sort implemented

--- Selection Sort.js
+++ Selection Sort.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const findSmallest = function (arr) {
+  let smallest = arr[0];
+  let smallest_index = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallest_index = i;
+    }
+  }
+
+  return smallest_index;
+};
+
+const selectionSort = function (arr) {
+  let newArr = [];
+  let notSorted = [...arr];
+
+  for (let i = 0; i < arr.length; i++) {
+    const smallest_index = findSmallest(notSorted);
+    newArr.push(...notSorted.splice(smallest_index, 1));
+  }
+
+  return newArr;
+};
